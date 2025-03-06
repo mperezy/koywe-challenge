4be8563f53f109ba040634f579eb3b5b7c775087
Manuel Perez <mperezy@unitru.edu.pe>
2025-03-05 23:35:17 -0500

    Update ESLint and Prettier both packages and configuration
    - Add package @nestjs/config to inject configuration service to get env varibles.

---
+++ eslint.config.mjs
@@ -0,0 +1,56 @@
+import eslint from '@eslint/js';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettier,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      ecmaVersion: 5,
+      sourceType: 'module',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/ban-types': 'off',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      semi: ['error', 'always'],
+
+      'max-len': [
+        'error',
+        {
+          code: 95,
+          ignoreComments: true,
+        },
+      ],
+
+      'no-console': 'error',
+
+      'no-trailing-spaces': [
+        'error',
+        {
+          skipBlankLines: true,
+        },
+      ],
+    },
+  }
+);
