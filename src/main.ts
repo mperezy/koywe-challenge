ab3540aede700504a54cd972907b5501132690e0
Manuel Perez <mperezy@unitru.edu.pe>
2025-03-06 20:58:48 -0500

    Add husky pre-commit hook

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'app.module';
 
+// eslint-disable-next-line
 (async () => {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
