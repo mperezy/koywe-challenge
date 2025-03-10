40f6b6d6978b295e75412eb2440d20cc2237440a
Manuel Perez <mperezy@unitru.edu.pe>
2025-03-10 00:01:29 -0500

    Update DB and Prisma schemas

--- db/scripts/quotes-db.sql
+++ db/scripts/quotes-db.sql
@@ -1,14 +1,15 @@
 CREATE SCHEMA IF NOT EXISTS quotes_db;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA quotes_db;
 
 SET search_path to quotes_db;
 
 CREATE TABLE IF NOT EXISTS quotes_db.quote (
-  id uuid PRIMARY KEY,
+  id uuid PRIMARY KEY DEFAULT public.uuid_generate_v4(),
   "from" TEXT NOT NULL,
   "to" TEXT NOT NULL,
   "amount" DECIMAL NOT NULL,
   "rate" DECIMAL NOT NULL,
   "convertedAmount" DECIMAL NOT NULL,
-  "timestamp" TIMESTAMP NOT NULL,
-  "expiresAt" Date NOT NULL
+  "timestamp" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  "expiresAt" TIMESTAMP NOT NULL
 );
